e57f023dda44a018ef83915154745891c10ceaff
pratik singh <prateeksingh26@outlook.com>
2025-06-06 03:06:36 +0530

    up

---
+++ frontend/src/home/rightpart/Right.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Right() { // Change 'right' to 'Right'
+  return <div className= 'w-[70%] bg-slate-900 text-gray-300'>Right</div>;
+}
+
+export default Right; // Change 'right' to 'Right'
